a9d9f6942aad225e15a7e11fdccce7437e9e00fb
Cmiszkur <cmiszkur@gmail.com>
2023-11-24 15:26:03 +0100

    viewing placed pawns

--- apps/carcassonne-client/src/app/game/room/playing-room/board/tile/tile.component.ts
+++ apps/carcassonne-client/src/app/game/room/playing-room/board/tile/tile.component.ts
@@ -7,7 +7,6 @@ import {
   Output,
   SimpleChanges,
 } from '@angular/core';
-import { TileEnvironments } from '../../../../models/Tile';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Pawn } from '../../../../models/pawn';
@@ -44,14 +43,6 @@ export class TileComponent extends BaseComponent implements OnChanges, OnInit {
    */
   @Input() public isTilePlacementConfirmed: boolean;
   /**
-   * Indicates confirmation of tile and pawn placement. Based on this variable tile is send to the server.
-   */
-  @Input() public istileAndPawnPlacementConfirmed: boolean;
-  /**
-   * Tile environments casted to object.
-   */
-  @Input() public tileEnvironments: TileEnvironments;
-  /**
    * Boolean that determines whether this tile is currently placed tile.
    */
   @Input() public isCurrentTile: boolean;
@@ -63,6 +54,9 @@ export class TileComponent extends BaseComponent implements OnChanges, OnInit {
    * Pawns corresponding to possible positions on the tile.
    */
   public pawns: Pawn[];
+
+  public pawn: Pawn | null;
+
   /**
    * Color of pawns of logged in player.
    */
@@ -78,12 +72,10 @@ export class TileComponent extends BaseComponent implements OnChanges, OnInit {
     this.rotation = 0;
     this.translate = '';
     this.isTilePlacementConfirmed = false;
-    this.istileAndPawnPlacementConfirmed = false;
-    this.tileEnvironments = {} as TileEnvironments;
     this.pawns = [];
+    this.pawn = null;
     this.isCurrentTile = false;
-    this.loggedPlayerColor =
-      this.roomService.playersValue?.loggedPlayer?.color || null;
+    this.loggedPlayerColor = this.roomService.playersValue?.loggedPlayer?.color || null;
     iconRegistry.addSvgIcon(
       'follower',
       sanitizer.bypassSecurityTrustResourceUrl('assets/SVG/follower.svg')
@@ -98,7 +90,14 @@ export class TileComponent extends BaseComponent implements OnChanges, OnInit {
   }
 
   ngOnInit() {
-    if (this.isCurrentTile) this.pawns = this.fillPossiblePawnPlacements();
+    if (this.isCurrentTile) {
+      this.pawns = this.fillPossiblePawnPlacements();
+    } else {
+      const fallowerDetails = this.extendedTile?.fallowerDetails;
+      this.pawn = fallowerDetails
+        ? this.generatePawn(fallowerDetails.position, fallowerDetails.placement)
+        : null;
+    }
   }
 
   public makeRotateTransformString(rotation: number): string {
@@ -145,18 +144,12 @@ export class TileComponent extends BaseComponent implements OnChanges, OnInit {
           Position.LEFT,
         ];
 
-        for (const [key, value] of Object.entries(tileValues) as [
-          Environment,
-          [Position[]]
-        ][]) {
+        for (const [key, value] of Object.entries(tileValues) as [Environment, [Position[]]][]) {
           value.forEach((values: Position[]) => {
             values.forEach((environmentValue, environmentIndex) => {
-              const environmentValuesIndex =
-                environmentValues.indexOf(environmentValue);
-              const environmentValuesIndexWithRotation =
-                (environmentValuesIndex + shiftValue) % 4;
-              values[environmentIndex] =
-                environmentValues[environmentValuesIndexWithRotation];
+              const environmentValuesIndex = environmentValues.indexOf(environmentValue);
+              const environmentValuesIndexWithRotation = (environmentValuesIndex + shiftValue) % 4;
+              values[environmentIndex] = environmentValues[environmentValuesIndexWithRotation];
             });
 
             if (values.length >= 2) {
@@ -204,5 +197,48 @@ export class TileComponent extends BaseComponent implements OnChanges, OnInit {
       }
     }
     return possiblePawnPlacements;
+  }
+
+  private generatePawn(values: Position[], key: Environment): Pawn | null {
+    if (!values.length) {
+      return null;
+    }
+
+    if (values.length >= 2) {
+      return {
+        transformValue: 'translate(32px, 32px)',
+        placement: key,
+        position: values,
+      };
+    }
+
+    switch (values[0]) {
+      case 'TOP':
+        return {
+          transformValue: 'translate(32px, 0)',
+          placement: key,
+          position: [Position.TOP],
+        };
+      case 'RIGHT':
+        return {
+          transformValue: 'translate(70px, 32px)',
+          placement: key,
+          position: [Position.RIGHT],
+        };
+      case 'BOTTOM':
+        return {
+          transformValue: 'translate(32px, 70px)',
+          placement: key,
+          position: [Position.BOTTOM],
+        };
+      case 'LEFT':
+        return {
+          transformValue: 'translate(0, 32px)',
+          placement: key,
+          position: [Position.LEFT],
+        };
+      default:
+        return null;
+    }
   }
 }
