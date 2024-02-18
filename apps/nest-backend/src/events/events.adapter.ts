da6033fbff512b2d29c135fbb1fc5cea0bb731b5
Cmiszkur <cmiszkur@gmail.com>
2024-02-18 12:52:11 +0100

    sockets https server

--- apps/nest-backend/src/events/events.adapter.ts
+++ apps/nest-backend/src/events/events.adapter.ts
@@ -1,3 +1,4 @@
+import { environment } from '@nest-backend/src/environments/environment.prod';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import passport = require('passport');
 import { Server, Socket, ServerOptions } from 'socket.io';
@@ -6,6 +7,8 @@ import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 import { WSAuthMiddleware } from './middleware/ws-auth.middleware';
 import { JwtService } from '@nestjs/jwt';
+import { createServer } from 'https';
+import * as fs from 'fs';
 
 export class SessionAdapter extends IoAdapter {
   private session: express.RequestHandler;
@@ -21,11 +24,14 @@ export class SessionAdapter extends IoAdapter {
 
   createIOServer(port: number, options?: ServerOptions): Server {
     const origin = this.configService.get<string>('NX_CLIENT_CORS_ORIGIN');
+    const cors = { origin, credentials: true };
     const path = this.configService.get<string>('NX_SOCKET_PATH');
-    options.cors = { origin, credentials: true };
+    options.cors = cors;
     options.path = path;
-    const server: Server = super.createIOServer(port, options);
 
+    const server = environment.production
+      ? this.createSecureIOServer(port, options)
+      : super.createIOServer(port, options);
     const wrap = (middleware) => (socket: Socket, next) => middleware(socket.request, {}, next);
 
     server.use(wrap(this.session));
@@ -34,5 +40,30 @@ export class SessionAdapter extends IoAdapter {
     server.use(wrap(cookieParser()));
     server.use(WSAuthMiddleware(this.jwtService));
     return server;
+  }
+
+  // create(
+  //   port: number,
+  //   options?: ServerOptions & { namespace?: string; server?: unknown }
+  // ): Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, unknown> {
+  //   const httpsServer = createServer({
+  //     ca: fs.readFileSync('/etc/ssl/server-cert-key/cloudflare.crt'),
+  //     key: fs.readFileSync('/etc/ssl/server-cert-key/key.pem'),
+  //     cert: fs.readFileSync('/etc/ssl/server-cert-key/cert.pem'),
+  //   }).listen(port);
+
+  //   const ioServer = new Server(httpsServer, options);
+
+  //   return super.create(port, environment.production ? { ...options, server: ioServer } : options);
+  // }
+
+  createSecureIOServer(port: number, options?: ServerOptions): Server {
+    const httpsServer = createServer({
+      ca: fs.readFileSync('/etc/ssl/server-cert-key/cloudflare.crt'),
+      key: fs.readFileSync('/etc/ssl/server-cert-key/key.pem'),
+      cert: fs.readFileSync('/etc/ssl/server-cert-key/cert.pem'),
+    }).listen(port);
+
+    return new Server(httpsServer, options);
   }
 }
